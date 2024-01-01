ef9945c051938b1d719a9c65ecc55fc4e2f10198
John Aricaya <jaricaya@gmail.com>
2023-12-31 16:03:58 -0800

    JVScript Event Listener Lab Completed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+//adding alert box after clicking button
+const input = document.getElementById('button');
+input.addEventListener('click',function() {
+    alert('I was clicked!');
+});
+
 }
